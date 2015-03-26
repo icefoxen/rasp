caa5ab8cbc0734e466744d066e8c86c5cd779640
Simon Heath <icefoxen@gmail.com>
2015-03-26 09:59:55 -0400

    Basic list data structure.  THINK it works, but needs Display trait.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
-pub mod rasp;
 
+
+pub enum Val {
+	Int(i32),
+	Cons(Cons),
+}
+
+pub enum Cons {
+	Nil,
+	Cell(Box<Val>, Box<Val>),
+}
+
+pub fn cons(car : Val, cdr : Val) -> Box<Cons> {
+	let ca = Box::new(car);
+	let cd = Box::new(cdr);
+	let cell = Cons::Cell(ca, cd);
+	Box::new(cell)
+}
 
 pub fn test() {
 	println!("Foo!");
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@ extern crate rasp;
 
 
 fn main() {
-    rasp::test();
+	rasp::test();
+    let cons = rasp::cons(rasp::Val::Int(10), rasp::Val::Int(20));
+    println!("Cons is {}", cons);
 }
