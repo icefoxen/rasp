e75af00ba73cc971db72bf6cc45ca3ee06944d65
Simon Heath <icefoxen@gmail.com>
2015-03-26 10:22:21 -0400

    borrow checkerrrrrrrr

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,31 @@ pub enum Val {
 	Cons(Cons),
 }
 
+impl std::fmt::Display for Val {
+	fn fmt(&self, formatter : &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+		match *self {
+			Val::Int(ref i)  => formatter.write_str(&format!("{}", i)),
+			Val::Cons(ref c) => formatter.write_str(&format!("{}", c)),
+		}
+	}
+}
+
+
 pub enum Cons {
 	Nil,
 	Cell(Box<Val>, Box<Val>),
 }
 
+
+impl std::fmt::Display for Cons {
+	fn fmt(&self, formatter : &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+		let formatted_str : &str = match *self {
+			Cons::Nil => "nil",
+			Cons::Cell(ref car, ref cdr) => &format!("({} . {})", car, cdr),
+		};
+		formatter.write_str(formatted_str)
+	}
+}
 pub fn cons(car : Val, cdr : Val) -> Box<Cons> {
 	let ca = Box::new(car);
 	let cd = Box::new(cdr);
