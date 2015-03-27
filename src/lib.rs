f87b605dc4ca20a8f4600e1bef10438de56c42ef
Simon Heath <icefoxen@gmail.com>
2015-03-27 01:14:51 -0400

    We can now print cons cells!
    Huzzah!

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,45 @@
-
+use std::fmt::*;
 
 pub enum Val {
 	Int(i32),
-	Cons(Cons),
+	Nil,
+	Cons(Box<Val>, Box<Val>),
+}
+
+fn start_write_cons(car : &Val, cdr : &Val, formatter : &mut std::fmt::Formatter) -> Result {
+	formatter.write_str(&format!("({}", car))
+		.and(
+	finish_write_cons(cdr, formatter)
+	)
+}
+
+fn finish_write_cons(val : &Val, formatter : &mut std::fmt::Formatter) -> Result {
+	match *val {
+		// If we get another cons cell, we write the first item, a space, then recurse
+		Val::Cons(ref car, ref cdr) => formatter.write_str(&format!(" {}", car)).and(finish_write_cons(cdr, formatter)),
+		// If we get nil, we end the list
+		Val::Nil => formatter.write_str(")"),
+		// If we get something else, we're finishing a dotted pair.
+		ref someval => formatter.write_str(&format!(" . {})", someval)),
+	}
 }
 
 impl std::fmt::Display for Val {
-	fn fmt(&self, formatter : &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+	fn fmt(&self, formatter : &mut std::fmt::Formatter) -> Result {
 		match *self {
 			Val::Int(ref i)  => formatter.write_str(&format!("{}", i)),
-			Val::Cons(ref c) => formatter.write_str(&format!("{}", c)),
+			Val::Nil => formatter.write_str("Nil"),
+			Val::Cons(ref car, ref cdr) => start_write_cons(car, cdr, formatter),
 		}
 	}
 }
 
-
-pub enum Cons {
-	Nil,
-	Cell(Box<Val>, Box<Val>),
-}
-
-
-impl std::fmt::Display for Cons {
-	fn fmt(&self, formatter : &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-		let formatted_str : &str = match *self {
-			Cons::Nil => "nil",
-			Cons::Cell(ref car, ref cdr) => &format!("({} . {})", car, cdr),
-		};
-		formatter.write_str(formatted_str)
-	}
-}
-pub fn cons(car : Val, cdr : Val) -> Box<Cons> {
+pub fn cons(car : Val, cdr : Val) -> Val {
 	let ca = Box::new(car);
 	let cd = Box::new(cdr);
-	let cell = Cons::Cell(ca, cd);
-	Box::new(cell)
+	let cell = Val::Cons(ca, cd);
+	cell
+	//Box::new(cell)
 }
 
 pub fn test() {
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate rasp;
 
 fn main() {
 	rasp::test();
-    let cons = rasp::cons(rasp::Val::Int(10), rasp::Val::Int(20));
+    let cons = rasp::cons(rasp::Val::Int(10), 
+    	rasp::cons(rasp::Val::Int(20), rasp::cons(rasp::Val::Int(30), rasp::Val::Nil)));
     println!("Cons is {}", cons);
 }
